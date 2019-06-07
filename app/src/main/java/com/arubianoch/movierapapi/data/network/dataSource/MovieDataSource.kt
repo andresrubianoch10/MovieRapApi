f585d7e59ef05e023c56195b5c883609a12ccf48
Andres Rubiano Del Chiaro <andres.rubiano.ch@gmail.com>
2019-06-06 22:55:32 -0500

    Added datasource to retrieve info from server

---
+++ app/src/main/java/com/arubianoch/movierapapi/data/network/dataSource/MovieDataSource.kt
@@ -0,0 +1,24 @@
+package com.arubianoch.movierapapi.data.network.dataSource
+
+import androidx.lifecycle.LiveData
+import com.arubianoch.movierapapi.data.network.response.MovieResponse
+
+/**
+ * @author Andres Rubiano Del Chiaro
+ */
+interface MovieDataSource {
+
+    val downloadedMovies: LiveData<MovieResponse>
+    val downloadedUpcomingMovies: LiveData<MovieResponse>
+
+    suspend fun fetchMovies(
+        sortBy: String,
+        movieType: String
+    )
+
+    suspend fun upcomingMovies(
+        initDate: String,
+        lastDate: String,
+        movieType: String
+    )
+}
---
+++ app/src/main/java/com/arubianoch/movierapapi/data/network/dataSource/MovieDataSourceImpl.kt
@@ -0,0 +1,44 @@
+package com.arubianoch.movierapapi.data.network.dataSource
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.arubianoch.movierapapi.data.network.ApiMovieService
+import com.arubianoch.movierapapi.data.network.response.MovieResponse
+import com.arubianoch.movierapapi.internal.exceptions.NoConnectivityException
+
+/**
+ * @author Andres Rubiano Del Chiaro
+ */
+class MovieDataSourceImpl(
+    private val apiMovieService: ApiMovieService
+) : MovieDataSource {
+
+    private val _downloadedMovies = MutableLiveData<MovieResponse>()
+    override val downloadedMovies: LiveData<MovieResponse>
+        get() = _downloadedMovies
+
+    private val _downloadedUpcomingMovies = MutableLiveData<MovieResponse>()
+    override val downloadedUpcomingMovies: LiveData<MovieResponse>
+        get() = _downloadedUpcomingMovies
+
+    override suspend fun fetchMovies(sortBy: String, movieType: String) {
+        try {
+            val fetchedMovies = apiMovieService.getPopularMovies(sortBy).await()
+            fetchedMovies.results.forEach { it.movieType = movieType}
+            _downloadedMovies.postValue(fetchedMovies)
+        } catch (e: NoConnectivityException) {
+            Log.e("Connectivity", "No internet connection.", e)
+        }
+    }
+
+    override suspend fun upcomingMovies(initDate: String, lastDate: String, movieType: String) {
+        try {
+            val upcomingMovies = apiMovieService.getUpcomingMovies(initDate, lastDate).await()
+            upcomingMovies.results.forEach { it.movieType = movieType }
+            _downloadedUpcomingMovies.postValue(upcomingMovies)
+        } catch (e: NoConnectivityException) {
+            Log.e("Connectivity", "No internet connection.", e)
+        }
+    }
+}
