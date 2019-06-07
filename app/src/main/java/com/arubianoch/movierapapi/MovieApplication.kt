11c6fa4a04d830aef79e4872313144875963d3d9
Andres Rubiano Del Chiaro <andres.rubiano.ch@gmail.com>
2019-06-06 22:50:24 -0500

    INjecting classes using Kodein

--- app/src/main/java/com/arubianoch/movierapapi/MovieApplication.kt
+++ app/src/main/java/com/arubianoch/movierapapi/MovieApplication.kt
@@ -1,8 +1,44 @@
 package com.arubianoch.movierapapi
 
 import android.app.Application
+import com.arubianoch.movierapapi.data.db.MovieDatabase
+import com.arubianoch.movierapapi.data.network.ApiMovieService
+import com.arubianoch.movierapapi.data.network.connectivity.ConnectivityInterceptor
+import com.arubianoch.movierapapi.data.network.connectivity.ConnectivityInterceptorImpl
+import com.arubianoch.movierapapi.data.network.dataSource.MovieDataSource
+import com.arubianoch.movierapapi.data.network.dataSource.MovieDataSourceImpl
+import com.arubianoch.movierapapi.data.repository.MovieRepository
+import com.arubianoch.movierapapi.data.repository.MovieRepositoryImpl
+import com.arubianoch.movierapapi.ui.popular.PopularViewModelFactory
+import com.jakewharton.threetenabp.AndroidThreeTen
+import org.kodein.di.Kodein
+import org.kodein.di.KodeinAware
+import org.kodein.di.android.x.androidXModule
+import org.kodein.di.generic.*
 
 /**
  * @author Andres Rubiano Del Chiaro
  */
-class MovieApplication : Application()
+class MovieApplication : Application(), KodeinAware {
+    override val kodein = Kodein.lazy {
+        import(androidXModule(this@MovieApplication))
+
+        bind() from singleton { MovieDatabase(instance()) }
+        bind() from singleton { instance<MovieDatabase>().movieDao() }
+        bind<ConnectivityInterceptor>() with singleton { ConnectivityInterceptorImpl(instance()) }
+        bind() from singleton { ApiMovieService(instance()) }
+        bind<MovieDataSource>() with singleton { MovieDataSourceImpl(instance()) }
+        bind<MovieRepository>() with singleton {
+            MovieRepositoryImpl(
+                instance(),
+                instance()
+            )
+        }
+        bind() from provider { PopularViewModelFactory(instance()) }
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        AndroidThreeTen.init(this)
+    }
+}
