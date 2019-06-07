63f005241750f14b8f9e73b0d8acee035bd1982f
Andres Rubiano Del Chiaro <andres.rubiano.ch@gmail.com>
2019-06-06 22:55:57 -0500

    Repository class that handle every call into database or server

---
+++ app/src/main/java/com/arubianoch/movierapapi/data/repository/MovieRepository.kt
@@ -0,0 +1,18 @@
+package com.arubianoch.movierapapi.data.repository
+
+import androidx.lifecycle.LiveData
+import com.arubianoch.movierapapi.data.db.entity.MovieInfo
+
+/**
+ * @author Andres Rubiano Del Chiaro
+ */
+interface MovieRepository {
+
+    suspend fun getMoviesByTopRated(): LiveData<List<MovieInfo>>
+
+    suspend fun getMoviesByPopular(): LiveData<List<MovieInfo>>
+
+    suspend fun getMoviesByUpcoming(): LiveData<List<MovieInfo>>
+
+    suspend fun getMovieDetail(): LiveData<MovieInfo>
+}
---
+++ app/src/main/java/com/arubianoch/movierapapi/data/repository/MovieRepositoryImpl.kt
@@ -0,0 +1,94 @@
+package com.arubianoch.movierapapi.data.repository
+
+import androidx.lifecycle.LiveData
+import com.arubianoch.movierapapi.data.db.dao.MovieDao
+import com.arubianoch.movierapapi.data.db.entity.MovieInfo
+import com.arubianoch.movierapapi.data.db.entity.MovieMetadata
+import com.arubianoch.movierapapi.data.network.dataSource.MovieDataSource
+import com.arubianoch.movierapapi.data.network.response.MovieResponse
+import kotlinx.coroutines.*
+import org.threeten.bp.ZonedDateTime
+
+/**
+ * @author Andres Rubiano Del Chiaro
+ */
+class MovieRepositoryImpl(
+    private val movieDao: MovieDao,
+    private val movieDataSource: MovieDataSource
+) : MovieRepository {
+
+    init {
+        movieDataSource.apply {
+            downloadedMovies.observeForever { persistMovies(it) }
+            downloadedUpcomingMovies.observeForever { persistMovies(it) }
+        }
+    }
+
+    private fun persistMovies(movies: MovieResponse?) {
+        GlobalScope.launch(Dispatchers.IO) {
+            movieDao.upsertMovieMetadata(
+                MovieMetadata(movies?.page, movies?.total_pages, movies?.total_results)
+            )
+            movieDao.upsertMovieInfo(movies?.results)
+        }
+    }
+
+    override suspend fun getMoviesByTopRated(): LiveData<List<MovieInfo>> {
+        initMovieData()
+        return withContext(Dispatchers.IO) {
+            return@withContext movieDao.getMoviesByType("topRated")
+        }
+    }
+
+    override suspend fun getMoviesByPopular(): LiveData<List<MovieInfo>> {
+        initMovieData()
+        return withContext(Dispatchers.IO) {
+            return@withContext movieDao.getMoviesByType("popular")
+        }
+    }
+
+    override suspend fun getMoviesByUpcoming(): LiveData<List<MovieInfo>> {
+        initMovieData()
+        return withContext(Dispatchers.IO) {
+            return@withContext movieDao.getMoviesByType("upcoming")
+        }
+    }
+
+    override suspend fun getMovieDetail(): LiveData<MovieInfo> {
+        return withContext(Dispatchers.IO) {
+            return@withContext movieDao.getMovieDetail(1)
+        }
+    }
+
+    private fun initMovieData() = runBlocking {
+        val lastInfoDownloaded: MovieMetadata? = movieDao.getMovieMetada()
+
+        if (lastInfoDownloaded == null) {
+            fetchTopRated(1)
+            fetchUpcoming("2019-06-15", "2019-09-15")
+            fetchPopular(1)
+        } else if (isFetchMovieNeeded(lastInfoDownloaded.zonedDateTime)) {
+            fetchTopRated(lastInfoDownloaded.page?.plus(1))
+            fetchUpcoming("2019-06-15", "2019-09-15")
+            fetchPopular(lastInfoDownloaded.page?.plus(1))
+        }
+
+    }
+
+    private suspend fun fetchPopular(page: Int?) {
+        movieDataSource.fetchMovies("popularity.desc", "popular")
+    }
+
+    private suspend fun fetchUpcoming(initDate: String, finishDate: String) {
+        movieDataSource.upcomingMovies(initDate, finishDate, "upcoming")
+    }
+
+    private suspend fun fetchTopRated(page: Int?) {
+        movieDataSource.fetchMovies("vote_average.desc", "topRated")
+    }
+
+    private fun isFetchMovieNeeded(lastFetchTime: ZonedDateTime): Boolean {
+        val thirtyMinutesAgo = ZonedDateTime.now().minusMinutes(30)
+        return lastFetchTime.isBefore(thirtyMinutesAgo)
+    }
+}
