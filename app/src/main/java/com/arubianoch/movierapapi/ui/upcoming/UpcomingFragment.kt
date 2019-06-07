4faea136d338fb090959df7d490d2b4f1cb0a863
Andres Rubiano Del Chiaro <andres.rubiano.ch@gmail.com>
2019-06-06 22:54:01 -0500

    Fetching upcoming  movies

--- app/src/main/java/com/arubianoch/movierapapi/ui/upcoming/UpcomingFragment.kt
+++ app/src/main/java/com/arubianoch/movierapapi/ui/upcoming/UpcomingFragment.kt
@@ -6,28 +6,66 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.GridLayoutManager
 
 import com.arubianoch.movierapapi.R
+import com.arubianoch.movierapapi.data.db.entity.MovieInfo
+import com.arubianoch.movierapapi.data.network.ApiMovieService
+import com.arubianoch.movierapapi.ui.adapter.MovieAdapter
+import com.arubianoch.movierapapi.ui.base.ScopedFragment
+import com.arubianoch.movierapapi.ui.popular.PopularViewModel
+import com.arubianoch.movierapapi.ui.popular.PopularViewModelFactory
+import kotlinx.android.synthetic.main.popular_fragment.*
+import kotlinx.android.synthetic.main.top_rated_fragment.*
+import kotlinx.android.synthetic.main.upcoming_fragment.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import org.kodein.di.KodeinAware
+import org.kodein.di.android.x.closestKodein
+import org.kodein.di.generic.instance
 
-class UpcomingFragment : Fragment() {
+class UpcomingFragment : ScopedFragment(), KodeinAware {
+
+    override val kodein by closestKodein()
+    private val viewModelFactory: PopularViewModelFactory by instance()
+    private var adapter: MovieAdapter? = null
+
+    private lateinit var viewModel: PopularViewModel
 
     companion object {
         fun newInstance() = UpcomingFragment()
     }
 
-    private lateinit var viewModel: UpcomingViewModel
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.upcoming_fragment, container, false)
+        return inflater.inflate(R.layout.popular_fragment, container, false)
+    }
+
+    private fun setUpRecycler(movies: List<MovieInfo>) {
+        adapter = MovieAdapter(activity!!, movies as ArrayList<MovieInfo>)
+        containerMovie.adapter = adapter
+        containerMovie.layoutManager = GridLayoutManager(activity!!, 2)
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(UpcomingViewModel::class.java)
-        // TODO: Use the ViewModel
+        viewModel = ViewModelProviders.of(activity!!, viewModelFactory).get(PopularViewModel::class.java)
+
+        bindUI()
     }
 
+    private fun bindUI() = launch(Dispatchers.Main) {
+        val movieResponse = viewModel.upcoming.await()
+
+        movieResponse.observe(viewLifecycleOwner, Observer { movies ->
+            if (movies.isNullOrEmpty()) return@Observer
+
+            group_loading.visibility = View.GONE
+            setUpRecycler(movies)
+        })
+    }
 }
