1cb65ce4a89e3ca72789b6a7c92a970beadd38ca
Andres Rubiano Del Chiaro <andres.rubiano.ch@gmail.com>
2019-06-06 22:51:17 -0500

    Created DAO to make query from local database

---
+++ app/src/main/java/com/arubianoch/movierapapi/data/db/dao/MovieDao.kt
@@ -0,0 +1,31 @@
+package com.arubianoch.movierapapi.data.db.dao
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.arubianoch.movierapapi.data.db.entity.MovieInfo
+import com.arubianoch.movierapapi.data.db.entity.MovieMetadata
+
+/**
+ * @author Andres Rubiano Del Chiaro
+ */
+@Dao
+interface MovieDao {
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    fun upsertMovieInfo(movieList: List<MovieInfo>?)
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    fun upsertMovieMetadata(movieList: MovieMetadata)
+
+    @Query("SELECT * FROM movie_info WHERE movieType = :movieType")
+    fun getMoviesByType(movieType: String): LiveData<List<MovieInfo>>
+
+    @Query("SELECT * FROM movie_info WHERE id = :movieId")
+    fun getMovieDetail(movieId: Int): LiveData<MovieInfo>
+
+    @Query("SELECT * FROM movie_metadata")
+    fun getMovieMetada(): MovieMetadata
+}
