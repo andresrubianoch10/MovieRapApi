9eecf85d10ac65317457359d11a88c7e13bf03ab
Andres Rubiano Del Chiaro <andres.rubiano.ch@gmail.com>
2019-06-10 00:16:50 -0500

    Injected new classes by KOdein

--- app/src/main/java/com/arubianoch/movierapapi/MovieApplication.kt
+++ app/src/main/java/com/arubianoch/movierapapi/MovieApplication.kt
@@ -9,12 +9,14 @@ import com.arubianoch.movierapapi.data.network.dataSource.MovieDataSource
 import com.arubianoch.movierapapi.data.network.dataSource.MovieDataSourceImpl
 import com.arubianoch.movierapapi.data.repository.MovieRepository
 import com.arubianoch.movierapapi.data.repository.MovieRepositoryImpl
+import com.arubianoch.movierapapi.ui.detailMovie.MovieDetailViewModelFactory
 import com.arubianoch.movierapapi.ui.popular.PopularViewModelFactory
 import com.jakewharton.threetenabp.AndroidThreeTen
 import org.kodein.di.Kodein
 import org.kodein.di.KodeinAware
 import org.kodein.di.android.x.androidXModule
 import org.kodein.di.generic.*
+import org.threeten.bp.LocalDate
 
 /**
  * @author Andres Rubiano Del Chiaro
@@ -35,6 +37,7 @@ class MovieApplication : Application(), KodeinAware {
             )
         }
         bind() from provider { PopularViewModelFactory(instance()) }
+        bind() from factory { movieId: String -> MovieDetailViewModelFactory(movieId, instance()) }
     }
 
     override fun onCreate() {
