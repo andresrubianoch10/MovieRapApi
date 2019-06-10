be4bd784a26ea630c4017e8d068f06f4187cdb3a
Andres Rubiano Del Chiaro <andres.rubiano.ch@gmail.com>
2019-06-10 00:19:05 -0500

    Aligned navigation controller with navigation action bar

--- app/src/main/java/com/arubianoch/movierapapi/ui/MainActivity.kt
+++ app/src/main/java/com/arubianoch/movierapapi/ui/MainActivity.kt
@@ -1,9 +1,10 @@
 package com.arubianoch.movierapapi.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupWithNavController
 import com.arubianoch.movierapapi.R
 import kotlinx.android.synthetic.main.activity_main.*
@@ -20,5 +21,11 @@ class MainActivity : AppCompatActivity() {
         navController = Navigation.findNavController(this, R.id.nav_host_fragment)
 
         bottom_nav.setupWithNavController(navController)
+
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(navController, null)
     }
 }
