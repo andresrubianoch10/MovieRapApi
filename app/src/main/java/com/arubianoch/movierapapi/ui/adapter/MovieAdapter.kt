a0c96111c139e2cb811e894a33065779fc1d27f7
Andres Rubiano Del Chiaro <andres.rubiano.ch@gmail.com>
2019-06-11 16:37:34 -0500

    Added some helper methods to retrieve relevant info from adapter

--- app/src/main/java/com/arubianoch/movierapapi/ui/adapter/MovieAdapter.kt
+++ app/src/main/java/com/arubianoch/movierapapi/ui/adapter/MovieAdapter.kt
@@ -15,9 +15,10 @@ import kotlinx.android.synthetic.main.item_movie.view.*
  */
 class MovieAdapter(
     private val context: Context,
-    private var data: ArrayList<MovieInfo>,
     private var onItemClickListener: OnItemClickListener
 ) : RecyclerView.Adapter<MovieAdapter.Holder>() {
+
+    private var data: ArrayList<MovieInfo>? = ArrayList()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
         val view = LayoutInflater.from(context).inflate(R.layout.item_movie, parent, false)
@@ -25,22 +26,48 @@ class MovieAdapter(
         return Holder(context, view, onItemClickListener, data)
     }
 
-    override fun getItemCount(): Int = data.size
-
     override fun onBindViewHolder(holder: Holder, position: Int) {
-        holder.bindView(data[position])
+        holder.bindView(data?.get(position))
     }
 
-    class Holder(private val context: Context,
+    override fun getItemCount(): Int {
+        return data?.size!!
+    }
+
+    private fun add(mc: MovieInfo) {
+        data?.add(mc)
+//        notifyItemInserted(data!!.size.minus(1))
+    }
+
+    fun addAll(mcList: List<MovieInfo>) {
+        if (mcList.size == data!!.size) {
+            notifyDataSetChanged()
+            return
+        }
+
+        val oldLength = data!!.size
+        val newLength = mcList.size
+
+        mcList.forEach {
+            if (!data?.contains(it)!!) {
+                add(it)
+            }
+        }
+
+        notifyItemRangeInserted(oldLength, newLength)
+    }
+
+    class Holder(
+        private val context: Context,
         itemView: View?,
         private val onItemClick: OnItemClickListener? = null,
-        data: ArrayList<MovieInfo>
+        data: ArrayList<MovieInfo>?
     ) :
         RecyclerView.ViewHolder(itemView!!) {
 
         init {
             itemView!!.setOnClickListener {
-                onItemClick?.onItemClicked(data[adapterPosition])
+                data?.get(adapterPosition)?.let { it1 -> onItemClick?.onItemClicked(it1) }
             }
         }
 
